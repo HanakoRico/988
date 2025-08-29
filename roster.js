f8d5c6f0763d27cd689a51d4cedff63e1e035cc9
Hanako <988c@mailum.com>
2025-08-29 17:30:27 -0500

    Create roster.js

---
+++ roster.js
@@ -0,0 +1,73 @@
+// roster.js
+const roster = {
+  "@2n9Hanako": {
+    role: "Owner/founder",
+    img: "images/hanako.jpg",
+    gif: "", // top banner gif (optional)
+    bio: "Hanako is the founder.",
+    links: ["https://twitter.com/hanako", "https://discord.gg/PxkPnxM2Hb"]
+  },
+  "@2n9Icey": {
+    role: "Member",
+    img: "images/icey.jpg",
+    gif: "",
+    bio: "Icey is the main editor.",
+    links: ["https://instagram.com/icey"]
+  },
+  "@2n9Melo": {
+    role: "Member",
+    img: "images/melo.jpg",
+    gif: "",
+    bio: "Melo is crazy.",
+    links: []
+  },
+  "@2n9Tecca": {
+    role: "Member",
+    img: "images/tecca.jpg",
+    gif: "",
+    bio: "Tecca is the artist.",
+    links: []
+  },
+  "@2n9FbiJr": {
+    role: "Member",
+    img: "images/fbijr.jpg",
+    gif: "",
+    bio: "FbiJr is special.",
+    links: []
+  },
+  "@2n9Slutting": {
+    role: "Member",
+    img: "images/slutting.jpg",
+    gif: "",
+    bio: "Slutting recruits.",
+    links: []
+  },
+  "@2n9Zox": {
+    role: "Member",
+    img: "images/zox.jpg",
+    gif: "",
+    bio: "Zox is energetic.",
+    links: []
+  },
+  "@2n9Bleeding": {
+    role: "Member",
+    img: "images/bleeding.jpg",
+    gif: "",
+    bio: "fedder | up-coming tool dev.",
+    links: []
+  },
+  "@2n9Juice12am": {
+    role: "Member",
+    img: "images/juice12am.jpg",
+    gif: "",
+    bio: "Juice is scary.",
+    links: []
+  },
+  "@2n9Banshee": {
+    role: "Member",
+    img: "images/banshee.jpg",
+    gif: "",
+    bio: "Banshee is fun.",
+    links: []
+  }
+};
